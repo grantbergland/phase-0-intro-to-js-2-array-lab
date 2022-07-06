75084901ab3582c1a61838984f455fbd79f6b5e8
grantbergland <grantbergland@gmail.com>
2022-07-06 14:50:26 -0500

    commit tree 0309ffc09614dbb8c408ebdcc1c68393c9b345cc

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat(name){
+cats.push(name);
+}
+
+function destructivelyPrependCat(name){
+cats.unshift(name);    
+}
+
+function destructivelyRemoveLastCat(){
+    cats.pop()
+}
+
+function destructivelyRemoveFirstCat(){
+    cats.shift()
+}
+
+function appendCat(name){
+    return [...cats, name]
+}
+
+function prependCat(name){
+    return [name, ...cats]
+}
+
+function removeLastCat(name) {
+    const cats = ["Milo", "Otis", "Garfield"];
+    cats.pop()
+    return cats
+}
+
+function removeFirstCat(name) {
+    const cats = ["Milo", "Otis", "Garfield"];
+    cats.shift()
+    return cats
+}
+
+
